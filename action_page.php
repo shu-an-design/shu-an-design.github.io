357e68a304d7c224f049ae342ad180422dec7376
shu-an-design <127091246+shu-an-design@users.noreply.github.com>
2023-09-12 16:57:25 +0800

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,19 +1,38 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["name"];
-  $email = $_POST["email"];
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-  // 郵件內容
-  $to = "yuchiao0129@gmail.com"; 
-  $subject = "New Form Submission";
-  $message = "Name: $name\n";
-  $message .= "Email: $email\n";
+require 'path/to/PHPMailer/PHPMailerAutoload.php';
 
-  // 發送郵件
-  if (mail($to, $subject, $message)) {
-    echo "郵件已成功發送！";
-  } else {
-    echo "郵件發送失敗。";
-  }
+// 创建一个新的PHPMailer实例
+$mail = new PHPMailer();
+
+// 邮件服务器设置
+$mail->isSMTP();                                      // 设置邮件使用SMTP协议
+$mail->Host = 'smtp.gmail.com';                       // 使用Google的SMTP服务器
+$mail->SMTPAuth = true;                               // 启用SMTP认证
+$mail->Username = 'your_gmail@gmail.com';            // 您的Gmail地址
+$mail->Password = 'your_gmail_password';              // 您的Gmail密码
+$mail->SMTPSecure = 'tls';                            // 启用TLS加密
+$mail->Port = 587;                                    // SMTP服务器的端口号
+
+// 邮件设置
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+$mail->setFrom($email, $name);  // 使用表单中提供的发件人地址和姓名
+$mail->addAddress('yuchiao0129@gmail.com', 'Recipient Name');   // 设置收件人地址和名称
+$mail->isHTML(true);                                  // 启用HTML格式
+
+// 邮件主题和内容
+$mail->Subject = '您收到一条新消息';
+$mail->Body    = '姓名：' . $name . '<br>邮箱：' . $email . '<br>消息：' . $_POST['message'];
+
+// 发送邮件
+if ($mail->send()) {
+    echo '邮件已成功发送';
+} else {
+    echo '邮件发送失败：' . $mail->ErrorInfo;
 }
 ?>
